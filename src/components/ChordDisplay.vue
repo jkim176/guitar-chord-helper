46e4304a4f49ba17917760dd62669c73fc6bacde
Jinsik Kim <chriskim4747@gmail.com>
2019-08-17 16:12:56 -0400

    a

--- src/components/ChordDisplay.vue
+++ src/components/ChordDisplay.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="chord-display">
 
-    <div class="chord-label">
-    </div>
-
     <div class="fret-label-container">
       <div v-for="(fret, index) in fretLabels"
            :key="fret.labelId"
@@ -17,8 +14,7 @@
       <div v-for="(fret, index) in firstString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: firstString[index].marker == 0,
-                    circle: firstString[index].marker == 1,
+           :class="{circle: firstString[index].marker == 1,
                     cross: firstString[index].marker == 2}"
       >
       </div>
@@ -28,8 +24,7 @@
       <div v-for="(fret, index) in secondString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: secondString[index].marker == 0,
-                    circle: secondString[index].marker == 1,
+           :class="{circle: secondString[index].marker == 1,
                     cross: secondString[index].marker == 2}"
       >
       </div>
@@ -39,8 +34,7 @@
       <div v-for="(fret, index) in thirdString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: thirdString[index].marker == 0,
-                    circle: thirdString[index].marker == 1,
+           :class="{circle: thirdString[index].marker == 1,
                     cross: thirdString[index].marker == 2}"
       >
       </div>
@@ -50,8 +44,7 @@
       <div v-for="(fret, index) in fourthString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: fourthString[index].marker == 0,
-                    circle: fourthString[index].marker == 1,
+           :class="{circle: fourthString[index].marker == 1,
                     cross: fourthString[index].marker == 2}"
       >
       </div>
@@ -61,8 +54,7 @@
       <div v-for="(fret, index) in fifthString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: fifthString[index].marker == 0,
-                    circle: fifthString[index].marker == 1,
+           :class="{circle: fifthString[index].marker == 1,
                     cross: fifthString[index].marker == 2}"
       >
       </div>
@@ -72,8 +64,7 @@
       <div v-for="(fret, index) in sixthString"
            :key="fret.cellId"
            class="fret"
-           :class="{blank: sixthString[index].marker == 0,
-                    circle: sixthString[index].marker == 1,
+           :class="{circle: sixthString[index].marker == 1,
                     cross: sixthString[index].marker == 2}"
       >
       </div>
@@ -155,10 +146,6 @@ export default {
 .chord-display {
   display: flex;
   flex-direction: column;
-
-}
-.chord-label {
-
 }
 .fret-label-container {
   display: flex;
@@ -194,9 +181,6 @@ export default {
 }
 .fret {
   flex: 1 1 50px;
-}
-.blank {
-  /* blank content */
 }
 .circle {
   background: black;
